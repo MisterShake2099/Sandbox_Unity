d9028dc83b7af3dfddf029e7f2108edcb3d8e91c
MisterShake2099 <mistershake2099@gmail.com>
2018-09-16 00:54:21 -0700

    Scripts to manage board setup, game state, and initial game start-up finished to a minimum functional state.

--- 2dRoguelikeTutorial/Assets/Scripts/GameManager.cs
+++ 2dRoguelikeTutorial/Assets/Scripts/GameManager.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public BoardManager BoardManager { get; private set; }
+
+    private readonly int level = 3;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        BoardManager = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        BoardManager.SetupScene(level);
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+    {
 		
 	}
 
